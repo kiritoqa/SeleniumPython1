22637c462e33d6938beb581657e7400931a36b7a
Сергей Фортуна <49685304+kiritoqa@users.noreply.github.com>
2020-03-11 23:10:24 +0200

    Update InitiateDriver.py

--- Base/InitiateDriver.py
+++ Base/InitiateDriver.py
@@ -2,7 +2,7 @@ from selenium.webdriver import Chrome
 from Library import ConfigReader
 def startBrowser():
     global driver
-    path = "./Driver/chromedriver.exe"
+    path = "C:/Python38/Scripts/chromedriver.exe"
     driver = Chrome(executable_path=path)
     driver.get(ConfigReader.readConfigData('Details', 'Application_URL'))
     driver.maximize_window()
